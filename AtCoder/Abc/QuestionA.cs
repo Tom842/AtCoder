82647aaa3bf09976a6972c2222a750a92a2bd341
Tomokazu Hashizume <tom.842e@gmail.com>
2020-05-10 21:26:58 +0900

    PracticeA - Welcome to AtCoder

--- AtCoder/Abc/QuestionA.cs
+++ AtCoder/Abc/QuestionA.cs
@@ -7,31 +7,33 @@ using System.Threading.Tasks;
 namespace AtCoder.Abc
 {
     //https://atcoder.jp/contests/abc141/tasks/abc141_a
-    public class QuestionA
+    public class QuestionA 
     {
+        /// <summary>
+        /// PracticeA - Welcome to AtCoder
+        /// https://atcoder.jp/contests/abs/tasks/practice_1
+        /// </summary>
+
+
         public static void Main(string[] args)
         {
             var sw = new System.IO.StreamWriter(Console.OpenStandardOutput()) { AutoFlush = false };
             Console.SetOut(sw);
 
-            // 文字列の入力
-            string s = Console.ReadLine();
-
             // 整数の入力
-            long n = long.Parse(Console.ReadLine());
-
-            // 文字列配列の入力
-            string[] inputStrArray = Console.ReadLine().Split(' ');
+            int num_a = int.Parse(Console.ReadLine());
 
             // 整数配列の入力
-            var inputLongArray = Console.ReadLine().Split(' ').Select(i => long.Parse(i)).ToArray();
+            var num_bc = Console.ReadLine().Split(' ').Select(i => int.Parse(i)).ToArray();
 
+            // 文字列の入力
+            string str_s = Console.ReadLine();
 
+            int num_Answer = num_a + num_bc[0] + num_bc[1];
 
+            string str_Answer = num_Answer.ToString() + " " + str_s;
 
-            string result = "";
-
-            Console.WriteLine(result);
+            Console.WriteLine(str_Answer );
 
             Console.Out.Flush();
         }

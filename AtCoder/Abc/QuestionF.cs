37cc03a9cbf48577425f279a5c83991e0c12c21b
Tomokazu Hashizume <tom.842e@gmail.com>
2020-06-02 22:50:12 +0900

    B - Homework

--- AtCoder/Abc/QuestionF.cs
+++ AtCoder/Abc/QuestionF.cs
@@ -8,29 +8,33 @@ namespace AtCoder.Abc
 {
     class QuestionF
     {
+        /// <summary>
+        /// B - Homework
+        /// https://atcoder.jp/contests/abc163/tasks/abc163_b
+        /// </summary>
         public static void Main(string[] args)
         {
             var sw = new System.IO.StreamWriter(Console.OpenStandardOutput()) { AutoFlush = false };
             Console.SetOut(sw);
 
-            // 文字列の入力
-            string s = Console.ReadLine();
-
-            // 整数の入力
-            long n = long.Parse(Console.ReadLine());
-
-            // 文字列配列の入力
-            string[] inputStrArray = Console.ReadLine().Split(' ');
+            // 整数配列の入力
+            var num_NM = Console.ReadLine().Split(' ').Select(i => int.Parse(i)).ToArray();  //num_NM[0]：N 日数　num_NM[1]：M 宿題数
 
             // 整数配列の入力
-            var inputLongArray = Console.ReadLine().Split(' ').Select(i => long.Parse(i)).ToArray();
+            var num_A = Console.ReadLine().Split(' ').Select(i => int.Parse(i)); //.ToArray();  //各宿題にかかる日数
 
+            int num_Sumday = num_A.Sum();　//LINQ Sum関数で宿題にかかる合計日数をもとめる
 
+            int num_Remain = num_NM[0] - num_Sumday;  //残り日数を求める
 
-
-            string result = "";
-
-            Console.WriteLine(result);
+            if (num_Remain >=0)
+            {
+                Console.WriteLine(num_Remain);
+            }
+            else if (num_Remain < 0)
+            {
+                Console.WriteLine("-1");
+            }
 
             Console.Out.Flush();
         }
